0d89044bde025b86544ebe5e6c55e5d278fadcf4
sil0613r <69302070+sil0613r@users.noreply.github.com>
2021-04-14 09:06:28 +0300

    Add files via upload

---
+++ how i wasted 5 minutes of my life/everything_is_txt.py
@@ -0,0 +1,17 @@
+import shutil
+import sys
+import random
+
+file_name = random.randint(0, 3)
+
+if file_name == 0:
+    file_name = 'why'
+elif file_name == 1:
+    file_name = 'gl reading that'
+elif file_name == 2:
+    file_name = '😭'
+elif file_name == 3:
+    file_name = 'pain'
+
+file_path = sys.argv[1] if len(sys.argv) > 1 else print('too many arguments')
+shutil.copy2(file_path, '%s.txt' % file_name)
