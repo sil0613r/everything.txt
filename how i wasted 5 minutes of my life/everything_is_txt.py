a50f4bcfadbcf1e194616f4891a1063a85e940ac
sil0613r <69302070+sil0613r@users.noreply.github.com>
2021-04-15 10:13:56 +0300

    Update everything_is_txt.py

--- how i wasted 5 minutes of my life/everything_is_txt.py
+++ how i wasted 5 minutes of my life/everything_is_txt.py
@@ -11,7 +11,7 @@ elif file_name == 1:
 elif file_name == 2:
     file_name = '😭'
 elif file_name == 3:
-    file_name = 'pain'
+    file_name = 'dont'
 
 file_path = sys.argv[1] if len(sys.argv) > 1 else print('too many arguments')
 shutil.copy2(file_path, '%s.txt' % file_name)
